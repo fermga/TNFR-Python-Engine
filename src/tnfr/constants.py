18db3703bf65b916a8f4b391008e5153bc77636c
fer <naiqun@gmx.com>
2025-08-23 16:09:19 +0200

    Delete src/tnfr/constants.py

--- src/tnfr/constants.py
+++
@@ -1,7 +0,0 @@
-glifo_categoria = {
-    "AL": "activador", "EN": "receptor", "IL": "estabilizador",
-    "OZ": "disonante", "UM": "acoplador", "RA": "resonador",
-    "SHA": "latente", "VAL": "expansivo", "NUL": "contractivo",
-    "THOL": "autoorganizador", "ZHIR": "mutante", "NAV": "transicional",
-    "REMESH": "recursivo"
-}
