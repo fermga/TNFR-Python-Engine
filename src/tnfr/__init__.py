2f7be8e171f93c8637e724d364471cebded5bb01
fer <naiqun@gmx.com>
2025-09-01 05:37:02 +0200

    Update __init__.py

--- src/tnfr/__init__.py
+++ src/tnfr/__init__.py
@@ -8,6 +8,8 @@ Ecuación nodal:
     ∂EPI/∂t = νf · ΔNFR(t)
 """
 
+__version__ = "4.5.2"
+
 # Re-exports de la API pública
 from .dynamics import step, run, set_delta_nfr_hook, validate_canon
 from .ontosim import preparar_red
@@ -84,6 +86,7 @@ __all__ = [
     "OPERADORES", "validate_sequence", "run_sequence",
     "__version__",
 ]
+
 
 
 
