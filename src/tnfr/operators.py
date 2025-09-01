e27b380afb0d628c2d593992e94946e958bdd224
fer <naiqun@gmx.com>
2025-09-01 13:04:56 +0200

    Use tuple-based checksum for node offset mapping

--- src/tnfr/operators.py
+++ src/tnfr/operators.py
@@ -5,6 +5,7 @@ import math
 import random
 import hashlib
 import heapq
+import pickle
 import networkx as nx
 from networkx.algorithms import community as nx_comm
 from functools import lru_cache
@@ -45,7 +46,7 @@ def _ensure_node_offset_map(G) -> Dict[Any, int]:
     """
 
     nodes = list(G.nodes())
-    checksum = hashlib.blake2b(",".join(map(str, nodes)).encode(), digest_size=16).hexdigest()
+    checksum = hashlib.blake2b(pickle.dumps(tuple(nodes)), digest_size=16).hexdigest()
     mapping = G.graph.get("_node_offset_map")
     if mapping is None or G.graph.get("_node_offset_checksum") != checksum:
         if bool(G.graph.get("SORT_NODES", False)):
