6634d05f4784cc0382048b2f8fd52da69890ad4a
fer <naiqun@gmx.com>
2025-09-02 09:53:59 +0200

    Limit ensure_collection materialization

--- tests/test_ensure_collection.py
+++ tests/test_ensure_collection.py
@@ -22,6 +22,18 @@ def test_max_materialize_limit():
         ensure_collection(gen, max_materialize=3)
 
 
+def test_default_limit_enforced():
+    gen = (i for i in range(1001))
+    with pytest.raises(ValueError):
+        ensure_collection(gen)
+
+
+def test_none_uses_default_limit():
+    gen = (i for i in range(1001))
+    with pytest.raises(ValueError):
+        ensure_collection(gen, max_materialize=None)
+
+
 def test_non_iterable_error():
     with pytest.raises(TypeError):
         ensure_collection(42)  # type: ignore[arg-type]
