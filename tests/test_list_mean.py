b08d47f6225debf19f674c40893d901a02904791
fmg <naiqun@protonmail.com>
2025-09-05 23:44:47 +0200

    Use statistics.fmean for list mean

--- tests/test_list_mean.py
+++ tests/test_list_mean.py
@@ -8,3 +8,11 @@ def test_list_mean_non_empty():
 
 def test_list_mean_empty_returns_default():
     assert list_mean([], default=5.0) == 5.0
+
+
+def test_list_mean_generator():
+    assert list_mean(x for x in (1.0, 2.0, 3.0)) == pytest.approx(2.0)
+
+
+def test_list_mean_empty_generator_returns_default():
+    assert list_mean((x for x in ()), default=3.3) == pytest.approx(3.3)
