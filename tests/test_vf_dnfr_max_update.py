24ef91d5ce2670b2da228a67e3edc81c28d81353
fmg <naiqun@protonmail.com>
2025-09-10 18:11:15 +0200

    Use direct assignment for vf and dnfr maxima

---
+++ tests/test_vf_dnfr_max_update.py
@@ -0,0 +1,23 @@
+import pytest
+
+from tnfr.alias import set_attr
+from tnfr.constants import ALIAS_VF, ALIAS_DNFR
+from tnfr.metrics_utils import _get_vf_dnfr_max
+
+
+def test_get_vf_dnfr_max_updates_graph_on_none(graph_canon):
+    G = graph_canon()
+    G.add_nodes_from([1, 2])
+    set_attr(G.nodes[1], ALIAS_VF, 0.5)
+    set_attr(G.nodes[2], ALIAS_VF, -1.5)
+    set_attr(G.nodes[1], ALIAS_DNFR, 0.2)
+    set_attr(G.nodes[2], ALIAS_DNFR, -0.4)
+    G.graph["_vfmax"] = None
+    G.graph["_dnfrmax"] = None
+
+    vfmax, dnfrmax = _get_vf_dnfr_max(G)
+
+    assert vfmax == pytest.approx(1.5)
+    assert dnfrmax == pytest.approx(0.4)
+    assert G.graph["_vfmax"] == pytest.approx(1.5)
+    assert G.graph["_dnfrmax"] == pytest.approx(0.4)
